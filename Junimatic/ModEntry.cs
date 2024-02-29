a45e8290a135e3bf52a136c5fe67cc87e6bb62b2
SteveBenz <s8878992@hotmail.com>
2024-02-28 20:47:15 -0800

    Empty Start

---
+++ Junimatic/ModEntry.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.GameData.Buildings;
+using StardewValley.GameData.GarbageCans;
+using StardewValley.GameData.Objects;
+using StardewValley.GameData.Tools;
+
+namespace NermNermNerm.Junimatic
+{
+    public class ModEntry
+        : Mod, ISimpleLog
+    {
+        public const string SpritesPseudoPath = "Mods/NermNermNerm/Junimatic/Sprites";
+
+        public Harmony Harmony = null!;
+
+        public ModEntry()
+        {
+        }
+
+        public override void Entry(IModHelper helper)
+        {
+            this.Harmony = new Harmony(this.ModManifest.UniqueID);
+
+            this.Helper.Events.GameLoop.OneSecondUpdateTicked += this.GameLoop_OneSecondUpdateTicked;
+        }
+
+        void ISimpleLog.WriteToLog(string message, LogLevel level, bool isOnceOnly)
+        {
+            if (isOnceOnly)
+            {
+                this.Monitor.LogOnce(message, level);
+            }
+            else
+            {
+                this.Monitor.Log(message, level);
+            }
+        }
+
+        private void GameLoop_OneSecondUpdateTicked(object? sender, OneSecondUpdateTickedEventArgs e)
+        {
+        }
+    }
+}
