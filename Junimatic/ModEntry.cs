e0c64bf2d48ab83794e8b268d16b8e6db3430aa9
SteveBenz <s8878992@hotmail.com>
2024-03-01 18:41:45 -0800

    Added a hut and a recipe for it

--- Junimatic/ModEntry.cs
+++ Junimatic/ModEntry.cs
@@ -8,6 +8,7 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using StardewValley.Buildings;
+using StardewValley.GameData.BigCraftables;
 using StardewValley.GameData.Buildings;
 using StardewValley.GameData.GarbageCans;
 using StardewValley.GameData.Objects;
@@ -32,8 +33,34 @@ namespace NermNermNerm.Junimatic
         {
             this.Harmony = new Harmony(this.ModManifest.UniqueID);
 
+            this.Helper.Events.Content.AssetRequested += this.OnAssetRequested;
+
             this.Helper.Events.GameLoop.OneSecondUpdateTicked += this.GameLoop_OneSecondUpdateTicked;
             this.Helper.Events.Input.ButtonPressed += this.Input_ButtonPressed;
+        }
+
+        private void OnAssetRequested(object? sender, AssetRequestedEventArgs e)
+        {
+            if (e.NameWithoutLocale.IsEquivalentTo(SpritesPseudoPath))
+            {
+                e.LoadFromModFile<Texture2D>("assets/Sprites.png", AssetLoadPriority.Exclusive);
+            }
+            else if (e.NameWithoutLocale.IsEquivalentTo("Data/BigCraftables"))
+            {
+                e.Edit(editor =>
+                {
+                    ObjectIds.EditAssets(editor.AsDictionary<string, BigCraftableData>().Data);
+                });
+            }
+            else if (e.NameWithoutLocale.IsEquivalentTo("Data/CraftingRecipes"))
+            {
+                // TODO: Make this recipe discovered
+                e.Edit(editor =>
+                {
+                    IDictionary<string, string> recipes = editor.AsDictionary<string, string>().Data;
+                    recipes[ObjectIds.JunimoPortal] = $"{StardewValley.Object.woodID} 20 {"92" /* sap*/} 30 {-777 /*wild seeds any*/} 5/Field/{ObjectIds.JunimoPortal}/true/Mining 0/";
+                });
+            }
         }
 
         private void Input_ButtonPressed(object? sender, ButtonPressedEventArgs e)
---
+++ Junimatic/ObjectIds.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StardewValley.GameData.BigCraftables;
+using StardewValley.GameData.Objects;
+
+namespace NermNermNerm.Junimatic
+{
+    public static class ObjectIds
+    {
+        public const string JunimoPortal = "NermNermNerm.Junimatic.JunimoPortal";
+
+        internal static void EditAssets(IDictionary<string, BigCraftableData> objects)
+        {
+            objects[JunimoPortal] = new BigCraftableData()
+            {
+                Name = JunimoPortal,
+                SpriteIndex = 0,
+                CanBePlacedIndoors = true,
+                CanBePlacedOutdoors = true,
+                Description = "A portal through which Junimos who want to help out on the farm can appear.  Place pathways next to these when placing them outdoors so the Junimos will know where to go.",
+                DisplayName = "Junimo Portal",
+                Texture = ModEntry.SpritesPseudoPath,
+            };
+        }
+    }
+}
