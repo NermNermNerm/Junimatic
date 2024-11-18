NermNermNerm/Junimatic

commit c70cd262e6638314020bfe2538ec9af8514dd5b2

Make Junimos treat rugs (the furniture) as bare floor

Junimatic/FlooringSet.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using StardewValley;
+using StardewValley.Objects;
 using StardewValley.TerrainFeatures;
 
 
@@ -44,15 +45,19 @@ namespace NermNermNerm.Junimatic
             {
                 return flooring.whichFloor.Value;
             }
-            else if (!l.IsOutdoors && l.isTilePassable(tile) && l.isTilePlaceable(tile) && l.getObjectAtTile(point.X, point.Y) is null)
+            else if (!l.IsOutdoors && l.isTilePassable(tile) && l.isTilePlaceable(tile))
             {
-                return I("#BARE_FLOOR#");
-            }
-            else
-            {
-                return null;
-            }
-        }
+                var objectAtLocation = l.getObjectAtTile(point.X, point.Y);
 
+                // Treat carpets as bare floor.
+                if (objectAtLocation is null
+                    || (objectAtLocation is Furniture f && f.furniture_type.Value == Furniture.rug))
+                {
+                    return I("#BARE_FLOOR#");
+                }
+            }
+
+            return null;
+        }
     }
 }

Junimatic/GameMap.cs
@@ -29,20 +29,22 @@ namespace NermNermNerm.Junimatic
         /// <param name="storage">If the tile contains a storage machine that the junimo could use, this is set.</param>
         public void GetThingAt(Point tileToCheck, Point reachableTile, FlooringSet validFloors, out bool isWalkable, out GameMachine? machine, out GameStorage? storage)
         {
+            if (validFloors.IsTileWalkable(this.location, tileToCheck))
+            {
+                isWalkable = true;
+                machine = null;
+                storage = null;
+                return;
+            }
+
+            // We check the IsTileWalkable before this because the item might be a rug, which is walkable, but
+            // otherwise would trip this case, which would cause us to stop working.
             var item = this.location.getObjectAtTile(tileToCheck.X, tileToCheck.Y);
             if (item is not null)
             {
                 isWalkable = false;
                 machine = ObjectMachine.TryCreate(item, reachableTile);
                 storage = GameStorage.TryCreate(item, reachableTile);
-                return;
-            }
-
-            if (validFloors.IsTileWalkable(this.location, tileToCheck))
-            {
-                isWalkable = true;
-                machine = null;
-                storage = null;
                 return;
             }
 

commit 6d95460035922068a8c783453293770a033d064c

Fix bug introduced by c70cd26 - machines placed on floors were ignored

Junimatic/FlooringSet.cs
@@ -40,21 +40,25 @@ namespace NermNermNerm.Junimatic
         internal static string? getFlooringId(GameLocation l, Point point)
         {
             Vector2 tile = point.ToVector2();
+
+            var objectAtLocation = l.getObjectAtTile(point.X, point.Y);
+
+            // If there's an object that's not a rug, it's not walkable
+            if (objectAtLocation is not null
+                && !(objectAtLocation is Furniture f && f.furniture_type.Value == Furniture.rug))
+            {
+                return null;
+            }
+
             l.terrainFeatures.TryGetValue(tile, out var terrainFeatures);
             if (terrainFeatures is Flooring flooring)
             {
+                // *assumes that if flooring can be placed there, then it must be passable...  Not sure how safe an assumption that is...
                 return flooring.whichFloor.Value;
             }
             else if (!l.IsOutdoors && l.isTilePassable(tile) && l.isTilePlaceable(tile))
             {
-                var objectAtLocation = l.getObjectAtTile(point.X, point.Y);
-
-                // Treat carpets as bare floor.
-                if (objectAtLocation is null
-                    || (objectAtLocation is Furniture f && f.furniture_type.Value == Furniture.rug))
-                {
-                    return I("#BARE_FLOOR#");
-                }
+                return I("#BARE_FLOOR#");
             }
 
             return null;